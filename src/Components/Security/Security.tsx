a7082de8896647f9e2e5b9b7a5388394ac904e5f
S.Sundar <114160338+sundarsaravanansk@users.noreply.github.com>
2024-07-06 09:55:50 +0530

    Add files via upload

---
+++ src/Components/Security/Security.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Security = () => {
+    return (
+        <div>
+            <div className='container mx-auto'>
+                <div>
+                    <div>
+                        <Image
+                        src={""}
+                        alt='security'
+                        width={800}
+                        height={800}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Security;
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import AboutSection from "@/Components/AboutSection/AboutSection";
+import IntroSection from "@/Components/IntroSection/IntroSection";
+import Security from "@/Components/Security/Security";
+
+
+
+export default function Home() {
+  return (
+  <>
+  <IntroSection/>
+  <Security/>
+  {/* <AboutSection/> */}
+  </>
+  );
+}
